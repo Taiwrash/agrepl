1553fe59232b8bb162284a958e3b484edf16b331
Rasheed Mudasiru <rasheedrtm1@gmail.com>
2026-04-17 21:04:36 +0100

    chore: configure github pages deployment with astro base path

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,5 +3,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://taiwrash.github.io',
+  base: '/agrepl',
   integrations: [mdx()],
 });

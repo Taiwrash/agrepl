48c38ba5b1d3e44d2f824cfd7e16eb7e44348c1d
Rasheed Mudasiru <rasheedrtm1@gmail.com>
2026-05-05 16:58:14 +0100

    updated cf dep

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://taiwrash.github.io',
   base: '/agrepl',
   integrations: [mdx()],
+  adapter: cloudflare(),
 });
